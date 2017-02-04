a4e5a87cdaab3c474c628e5ac80571f601c7c4bf
KamilMichalski <michalski.kamil@gmail.com>
2017-02-04 18:55:51 +0100

    first commit

---
+++ src/main/java/michalski/kamil/TweetSearchApplication.java
@@ -0,0 +1,12 @@
+package michalski.kamil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TweetSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TweetSearchApplication.class, args);
+	}
+}
---
+++ src/test/java/michalski/kamil/TweetSearchApplicationTests.java
@@ -0,0 +1,16 @@
+package michalski.kamil;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TweetSearchApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
