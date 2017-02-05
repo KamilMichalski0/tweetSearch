97a6699fbd93ed161a5d4afefba5b43bea1dda38
KamilMichalski <michalski.kamil@gmail.com>
2017-02-05 12:46:00 +0100

    added WebConfiguration and USLocalDateFormatter class

--- src/main/java/michalski/kamil/controller/ProfileController.java
+++ src/main/java/michalski/kamil/controller/ProfileController.java
@@ -4,10 +4,12 @@ package michalski.kamil.controller;
 import michalski.kamil.profile.ProfileForm;
 import michalski.kamil.uSLocalDateFormatter.USLocalDateFormatter;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.validation.Valid;
 import java.util.Locale;
 
 @Controller
@@ -18,7 +20,11 @@ public class ProfileController {
     }
 
     @RequestMapping(value = "/profile", method = RequestMethod.POST)
-    public String saveProfile(ProfileForm profileForm) {
+    public String saveProfile(@Valid ProfileForm profileForm,
+                              BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "profile/profilePage";
+        }
         System.out.println("pomyślnie zapisany profil " + profileForm);
         return "redirect:/profile";
     }
--- src/main/java/michalski/kamil/profile/ProfileForm.java
+++ src/main/java/michalski/kamil/profile/ProfileForm.java
@@ -1,14 +1,24 @@
 package michalski.kamil.profile;
 
 
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ProfileForm {
+    @Size(min = 2)
     private String twitterHandle;
+    @Email
+    @NotEmpty
     private String email;
+    @NotNull
     private LocalDate birthDate;
+    @NotEmpty
     private List<String> tastes = new ArrayList<>();
 
     public String getTwitterHandle() {
