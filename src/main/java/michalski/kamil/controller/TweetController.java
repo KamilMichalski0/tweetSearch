e861ae1c4f86df0a67b34f0b2f04336b4aaef695
KamilMichalski <michalski.kamil@gmail.com>
2017-02-05 10:09:08 +0100

    added materializecss and jquery

--- src/main/java/michalski/kamil/controller/TweetController.java
+++ src/main/java/michalski/kamil/controller/TweetController.java
@@ -2,11 +2,17 @@ package michalski.kamil.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.social.twitter.api.SearchResults;
+import org.springframework.social.twitter.api.Tweet;
 import org.springframework.social.twitter.api.Twitter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.awt.SystemColor.text;
 
 @Controller
 public class TweetController {
@@ -17,8 +23,11 @@ public class TweetController {
     public String hello(@RequestParam(defaultValue = "SpringJestFajny") String search,
                         Model model) {
         SearchResults searchResults = twitter.searchOperations().search(search);
-        String text = searchResults.getTweets().get(0).getText();
-        model.addAttribute("message", text);
+        List<String> tweets = searchResults.getTweets()
+                .stream()
+                .map(Tweet::getText)
+                .collect(Collectors.toList());
+        model.addAttribute("tweets", tweets);
         return "resultPage";
     }
 }
