dd1db604f7a07ad5f44f26321669d507d2171365
KamilMichalski <michalski.kamil@gmail.com>
2017-02-06 14:08:25 +0100

    added uploadPage and PictureUploadController

--- src/main/java/michalski/kamil/controller/PictureUploadController.java
+++ src/main/java/michalski/kamil/controller/PictureUploadController.java
@@ -1,7 +1,53 @@
 package michalski.kamil.controller;
 
-/**
- * Created by ppg38 on 06.02.2017.
- */
+import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.io.*;
+
+
+@Controller
 public class PictureUploadController {
+
+    public static final Resource PICTURES_DIR = new FileSystemResource("./pictures");
+
+    @RequestMapping("upload")
+    public String uploadPage() {
+        return "profile/uploadPage";
+    }
+
+    @RequestMapping(value = "/upload", method = RequestMethod.POST)
+    public String onUpload(MultipartFile file, RedirectAttributes redirectAttrs)
+            throws IOException {
+        if (file.isEmpty() || !isImage(file)) {
+            redirectAttrs.addFlashAttribute("error",
+                    "Niewłaściwy plik. Załaduj plik z obrazem.");
+            return "redirect:/upload";
+        }
+        copyFileToPictures(file);
+        return "profile/uploadPage";
+    }
+
+    private Resource copyFileToPictures(MultipartFile file) throws IOException {
+        String fileExtension = getFileExtension(file.getOriginalFilename());
+        File tempFile = File.createTempFile("pic", fileExtension, PICTURES_DIR.getFile());
+        try (InputStream in = file.getInputStream();
+             OutputStream out = new FileOutputStream(tempFile)) {
+            IOUtils.copy(in, out);
+        }
+        return new FileSystemResource(tempFile);
+    }
+    private boolean isImage(MultipartFile file) {
+        return file.getContentType().startsWith("image");
+    }
+
+    private static String getFileExtension(String name) {
+        return name.substring(name.lastIndexOf("."));
+    }
 }
--- src/main/java/michalski/kamil/controller/ProfileController.java
+++ src/main/java/michalski/kamil/controller/ProfileController.java
@@ -9,19 +9,27 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.util.Locale;
 
 @Controller
 public class ProfileController {
+
+    @ModelAttribute("dateFormat")
+    public String localeFormat(Locale locale) {
+        return USLocalDateFormatter.getPattern(locale);
+    }
+
     @RequestMapping("/profile")
     public String displayProfile(ProfileForm profileForm) {
         return "profile/profilePage";
     }
 
-    @RequestMapping(value = "/profile", method = RequestMethod.POST)
-    public String saveProfile(@Valid ProfileForm profileForm,
-                              BindingResult bindingResult) {
+    @RequestMapping(value = "/profile", params = {"save"}, method =
+            RequestMethod.POST)
+    public String saveProfile(@Valid ProfileForm profileForm, BindingResult
+            bindingResult) {
         if (bindingResult.hasErrors()) {
             return "profile/profilePage";
         }
@@ -29,8 +37,18 @@ public class ProfileController {
         return "redirect:/profile";
     }
 
-    @ModelAttribute("dateFormat")
-    public String localeFormat(Locale locale) {
-        return USLocalDateFormatter.getPattern(locale);
+    @RequestMapping(value = "/profile", params = {"addTaste"})
+    public String addRow(ProfileForm profileForm) {
+        profileForm.getTastes().add(null);
+        return "profile/profilePage";
+    }
+
+    @RequestMapping(value = "/profile", params = {"removeTaste"})
+    public String removeRow(ProfileForm profileForm, HttpServletRequest req) {
+        Integer rowId = Integer.valueOf(req.getParameter("removeTaste"));
+        profileForm.getTastes().remove(rowId.intValue());
+        return "profile/profilePage";
     }
 }
+
+
