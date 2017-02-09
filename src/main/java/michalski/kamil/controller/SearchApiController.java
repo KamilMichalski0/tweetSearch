0489033711ee43fa6e495075bbf23cdf2330944d
KamilMichalski <michalski.kamil@gmail.com>
2017-02-09 22:22:19 +0100

    Added SearchApiController

---
+++ src/main/java/michalski/kamil/controller/SearchApiController.java
@@ -0,0 +1,27 @@
+package michalski.kamil.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.social.twitter.api.Tweet;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/search")
+public class SearchApiController {
+
+
+    private SearchService searchService;
+
+    @Autowired
+    public SearchApiController(SearchService searchService) {
+        this.searchService = searchService;
+    }
+
+    @RequestMapping(value = "/{searchType}", method = RequestMethod.GET)
+    public List<Tweet> search(@PathVariable String searchType,
+                              @MatrixVariable List<String> keywords) {
+        return searchService.search(searchType, keywords);
+    }
+}
